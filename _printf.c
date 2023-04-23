06ffa3ab74b4d2a686e835a5d2be02d2ebe7b8f0
hariti asmae <haritiasmaa74@gmail.com>
2023-04-23 10:25:25 -0700

    prinf2

---
+++ _printf.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    int count = 0;
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            if (*format == 'c') {
+                int c = va_arg(args, int);
+                putchar(c);
+                count++;
+            } else if (*format == 's') {
+                char *s = va_arg(args, char*);
+                while (*s != '\0') {
+                    putchar(*s);
+                    s++;
+                    count++;
+                }
+            } else if (*format == '%') {
+                putchar('%');
+                count++;
+            }
+        } else {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+    va_end(args);
+    return count;
+}
+
