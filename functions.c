c3b827867cb1119037bb8dcdb89f9ce844533636
hariti asmae <72623740+hariti-asm@users.noreply.github.com>
2023-11-12 22:18:51 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -9,16 +9,7 @@ int print_char(va_list types, char buffer[],
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 /************************* PRINT A STRING *************************/
-/**
- * print_string - Prints a string
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
- */
+
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
